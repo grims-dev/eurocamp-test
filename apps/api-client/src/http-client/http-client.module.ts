21fe1dd9cde83184e321f207112e187f10c03648
grims-dev <ciarangrimshawdev@gmail.com>
2026-08-18 22:16:44 +0100

    HTTP Client service/module boilerplate

---
+++ apps/api-client/src/http-client/http-client.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { HttpClientService } from './http-client.service';
+
+@Module({
+  providers: [HttpClientService],
+})
+export class HttpClientModule {}
