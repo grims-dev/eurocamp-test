82bc58679f95c52b9125f334c48429e0ba53b97a
grims-dev <ciarangrimshawdev@gmail.com>
2026-08-19 20:10:22 +0100

    Init Tanstack QueryClientProvider

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,17 +1,20 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './styles.css';
+
+const queryClient = new QueryClient();
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Head>
-        <title>Welcome to frontend!</title>
+        <title>Eurocamp</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
       </main>
-    </>
+    </QueryClientProvider>
   );
 }
 
